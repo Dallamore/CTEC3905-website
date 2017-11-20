f4bc0bcad2a3fa38eedef936ebaec108b57e555f
Safrapath <P15188966@my365.dmu.ac.uk>
2017-11-20 16:19:31 +0000

    Working bgSwap

---
+++ scripts/bg-swap.js
@@ -0,0 +1,16 @@
+if (window.attachEvent) {window.attachEvent('onload', bgSwap);}
+else if (window.addEventListener) {window.addEventListener('load', bgSwap, false);}
+else {document.addEventListener('load', bgSwap, false);}
+
+document.getElementById("background").addEventListener("onload", bgSwap,false);
+
+function bgSwap()
+{
+	var totalCount = 5;
+	var num = Math.ceil( Math.random() * totalCount );
+	document.body.background = 'bgimages/'+num+'.jpg';
+	document.body.style.backgroundSize = "cover";// Background repeat
+
+	document.body.style.position = "center";
+
+}
