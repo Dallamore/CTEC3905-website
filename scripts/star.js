fc9642d9e89cd73d7ee51bc6e2e14497cd3961ce
Safrapath <P15188966@my365.dmu.ac.uk>
2017-12-05 15:25:07 +0000

    Started content planning

--- scripts/star.js
+++ scripts/star.js
@@ -8,11 +8,11 @@ let svgns = "http://www.w3.org/2000/svg";
 stArray =[];
 
 for (i = 0; i < 8000; i++){
-  stArray[i] = document.createElementNS(svgns, "circle");
-  stArray[i].setAttributeNS(null, "cx", getRandomInt(0,4000)); //width
-  stArray[i].setAttributeNS(null, "cy", getRandomInt(0,3000)); //height
-  stArray[i].setAttributeNS(null, "r", getRandomInt(1,2)); //
-  stArray[i].setAttributeNS(null, "fill", "white");
+	stArray[i] = document.createElementNS(svgns, "circle");
+	stArray[i].setAttributeNS(null, "cx", getRandomInt(0,4000)); //width
+	stArray[i].setAttributeNS(null, "cy", getRandomInt(0,3000)); //height
+	stArray[i].setAttributeNS(null, "r", getRandomInt(1,2)); //
+	stArray[i].setAttributeNS(null, "fill", "white");
 	stArray[i].setAttributeNS(null, "class", "shooting");
 }
 
