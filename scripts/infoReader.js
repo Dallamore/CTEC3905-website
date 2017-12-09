3823c4661b0f8e3aa09f9aa5c47c487b365437fb
Safrapath <P15188966@my365.dmu.ac.uk>
2017-12-09 16:33:49 +0000

    started adding json

--- scripts/infoReader.js
+++ scripts/infoReader.js
@@ -1,21 +1,20 @@
-
-
 let div = document.getElementsByClassName('news-story')[0];
 let h5 = div.getElementsByTagName('h5');
 let p = div.getElementsByTagName('p');
-// let data = {
-//         "items": [
-//             {
-//             "title": "sample 1",
-//             "author": "author 1"},
-//         {
-//             "title": "sample 2",
-//             "author": "author 2"}
-//         ]
-//     };
 
-let json = require('/content.json');
-console.log(json);
+let imgHolder = document.getElementById("imgHolder");
+
+
+let data = {
+        "items": [
+            {
+            "title": "sample 1",
+            "author": "author 1"},
+        {
+            "title": "sample 2",
+            "author": "author 2"}
+        ]
+    };
 
 
 data.items.forEach(function(v, i) {
